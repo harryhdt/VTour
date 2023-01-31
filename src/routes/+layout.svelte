4dd21eab9e17ce61fa5dee7423928567171018bc
Harry Hidayat <harryhdtofficial@gmail.com>
2023-01-31 07:40:56 +0700

    Test 1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script>
+	import { fade } from 'svelte/transition';
+	import { navigating } from '$app/stores';
 	import '$src/app.css';
 </script>
 
+{#if $navigating}
+	<div
+		transition:fade={{ duration: 200 }}
+		class="fixed inset-0 z-[9999] bg-white bg-opacity-20 backdrop-blur-sm md:backdrop-blur-none md:bg-transparent md:cursor-wait"
+	/>
+{/if}
 <div id="app" class="min-h-screen flex flex-col p-4 md:p-6 font-lato">
 	<main>
 		<slot />
